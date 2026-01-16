6e461d79aacb3089777d45274a39df0fc2a41d1f
HHVVM1 <jfr715024@gmail.com>
2026-01-16 14:44:48 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .sendpy import token, id, send
+
+__version__ = "1.0.0"
+__all__ = ['token', 'id', 'send']
---
+++ example.py
@@ -0,0 +1,6 @@
+import sendpy
+t = sendpy.token("token : ")
+i = sendpy.id("id : ")
+msg = "نص الرسالة هنا"
+
+sendpy.send(msg=msg, id=i, token=t)
